229c1ea30fc38ac3ad6dda3e5685839fbf8012c8
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2023-07-24 23:23:41 +0200

    continue work on docker image but now get a ___ROOT___#build when using the turbo build

--- apps/awaken-overlay/vite.config.ts
+++ apps/awaken-overlay/vite.config.ts
@@ -2,9 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { vitePluginTrpcWebSocket } from 'trpc-sveltekit/websocket';
 
+/** @type (import('vite').UserConfig) */
 export default defineConfig({
-	plugins: [sveltekit(), vitePluginTrpcWebSocket],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [sveltekit(), vitePluginTrpcWebSocket],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  }
 });
