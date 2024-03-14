9002b86aa589c2180c41065141d7f40a43b2bb0a
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-03-14 21:06:28 +0100

    add working overlay but still need some ajustments to correctly align bo scores and change text title to include BO name

--- apps/awaken-overlay/src/lib/components/scoreUpdate.svelte
+++ apps/awaken-overlay/src/lib/components/scoreUpdate.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { currentMatch } from '$lib/stores/currentMatchStore';
-  import { Input, Button } from 'flowbite-svelte';
+  import { Input, Button, Tabs, TabItem } from 'flowbite-svelte';
   import TeamSelect from '$lib/components/TeamSelect.svelte';
   import ScorePlus from './ScorePlus.svelte';
   import { createEventDispatcher } from 'svelte';
@@ -24,38 +24,54 @@
 
 {#if !error}
   <div class="flex flex-col text-center w-fit pl-5">
-    <div>
-      <ScorePlus
-        score={$currentMatch.team1Score}
-        isLoading={loading}
-        on:scoreUpdate={(e) => ($currentMatch.team1Score = e.detail)}
-        ><p class="text-green-400">Team1 Score</p></ScorePlus
-      >
-      <TeamSelect {teams} selectedTeam={$currentMatch.team1Id} class="mt-5 max-w-12" />
-    </div>
-    <div class="mt-5">
-      <ScorePlus
-        score={$currentMatch.team2Score}
-        isLoading={loading}
-        on:scoreUpdate={(e) => ($currentMatch.team2Score = e.detail)}
-        ><p class="text-blue-700">Team2 Score</p></ScorePlus
-      >
-      <TeamSelect {teams} selectedTeam={$currentMatch.team2Id} class="mt-5" />
-    </div>
-    <div class="mt-5">
-      <p class="text-red-400">BO Format</p>
-      <BoSelect
-        {bos}
-        selectedBo={$currentMatch.bestOfId}
-        on:boUpdate={(e) => ($currentMatch.bestOfId = e.detail)}
-        class="mt-5"
-      />
-    </div>
-    <div class="mt-5">
-      <p class="text-red-400">Game title</p>
-      <Input type="text" bind:value={localTitle} class="mt-5" />
-      <Button on:click={() => ($currentMatch.gameTitle = localTitle)}>Change</Button>
-    </div>
+    <Tabs>
+      <TabItem open title="Scores">
+        <div>
+          <ScorePlus
+            score={$currentMatch.team1Score}
+            isLoading={loading}
+            on:scoreUpdate={(e) => ($currentMatch.team1Score = e.detail)}
+            ><p class="text-green-400">Team1 Score</p></ScorePlus
+          >
+          <TeamSelect
+            {teams}
+            selectedTeam={$currentMatch.team1Id}
+            on:teamUpdate={(e) => ($currentMatch.team1Id = e.detail)}
+            class="mt-5 max-w-12"
+          />
+        </div>
+        <div class="mt-5">
+          <ScorePlus
+            score={$currentMatch.team2Score}
+            isLoading={loading}
+            on:scoreUpdate={(e) => ($currentMatch.team2Score = e.detail)}
+            ><p class="text-blue-700">Team2 Score</p></ScorePlus
+          >
+          <TeamSelect
+            {teams}
+            selectedTeam={$currentMatch.team2Id}
+            on:teamUpdate={(e) => ($currentMatch.team2Id = e.detail)}
+            ss="mt-5"
+          />
+        </div>
+      </TabItem>
+      <TabItem title="Game format">
+        <div class="mt-5">
+          <p class="text-red-400">BO Format</p>
+          <BoSelect
+            {bos}
+            selectedBo={$currentMatch.bestOfId}
+            on:boUpdate={(e) => ($currentMatch.bestOfId = e.detail)}
+            class="mt-5"
+          />
+        </div>
+        <div class="mt-5">
+          <p class="text-red-400">Game title</p>
+          <Input type="text" bind:value={localTitle} class="mt-5" />
+          <Button on:click={() => ($currentMatch.gameTitle = localTitle)}>Change</Button>
+        </div>
+      </TabItem>
+    </Tabs>
   </div>
 {:else}
   <p>{error}</p>
--- apps/awaken-overlay/src/routes/(overlay)/overlay/+page.svelte
+++ apps/awaken-overlay/src/routes/(overlay)/overlay/+page.svelte
@@ -1,147 +1,181 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { gameStore } from '$lib/stores/gameStore';
-	import { trpc } from '$lib/trpc/client';
-	import { SosWebSocket } from '$lib/sos/client';
-	import Timer from '$lib/components/Timer.svelte';
-	import type { CurrentMatchWithTeams } from '$lib/currentMatch.model.server';
+  import { onMount } from 'svelte';
+  import { gameStore } from '$lib/stores/gameStore';
+  import { trpc } from '$lib/trpc/client';
+  import { SosWebSocket } from '$lib/sos/client';
+  import Timer from '$lib/components/Timer.svelte';
+  import type { CurrentMatchWithTeams } from '$lib/currentMatch.model.server';
 
-	let maxGames = 4;
+  const client = trpc();
+  let ws: SosWebSocket;
 
-	const client = trpc();
-	let ws: SosWebSocket;
+  const updateStore = (newData: Omit<CurrentMatchWithTeams, 'createdAt' | 'updatedAt'>) => {
+    $gameStore.serie.team1Score = newData.team1Score;
+    $gameStore.serie.team2Score = newData.team2Score;
+    $gameStore.icons.team1 = newData.team1.leftIcon;
+    $gameStore.icons.team2 = newData.team2.rightIcon;
+    $gameStore.game.title = newData.gameTitle;
+    $gameStore.serie.nbOfMatch = newData.bestOf.nbOfMatch;
+  };
+  onMount(async () => {
+    ws = new SosWebSocket();
+    ws.subscribe('game:update_state', (data) => {
+      $gameStore.game.timeLeft = data.game.time_seconds;
+      $gameStore.game.team1Score = data.game.teams[0].score;
+      $gameStore.game.team2Score = data.game.teams[1].score;
+    });
+    const newData = await client.getCurrentMatch.query();
+    updateStore(newData);
 
-	const updateStore = (newData: Omit<CurrentMatchWithTeams, 'createdAt' | 'updatedAt'>) => {
-		$gameStore.serie.team1Score = newData.team1Score;
-		$gameStore.serie.team2Score = newData.team2Score;
-		$gameStore.icons.team1 = newData.team1.leftIcon;
-		$gameStore.icons.team2 = newData.team2.rightIcon;
-		$gameStore.game.title = newData.gameTitle;
-		$gameStore.serie.nbOfMatch = newData.bestOf.nbOfMatch;
-
-	};
-	onMount(async () => {
-		ws = new SosWebSocket();
-		ws.subscribe('game:update_state', (data) => {
-			$gameStore.game.timeLeft = data.game.time_seconds;
-			$gameStore.game.team1Score = data.game.teams[0].score;
-			$gameStore.game.team2Score = data.game.teams[1].score;
-		});
-		const newData = await client.getCurrentMatch.query();
-		updateStore(newData);
-
-		client.currentTeamUpdate.subscribe(undefined, {
-			onData(newData) {
-				updateStore(newData);
-			}
-		});
-		document.body.style.padding = '0px';
-		document.body.style.margin = '0px';
-	});
+    client.currentTeamUpdate.subscribe(undefined, {
+      onData(newData) {
+        updateStore(newData);
+      }
+    });
+    document.body.style.padding = '0px';
+    document.body.style.margin = '0px';
+  });
 </script>
 
 <div id="container">
-	<div class="scores">
-		<div class="image">
-			<img src={$gameStore.icons.team1} alt="Team 1 Logo" />
-		</div>
-		<div class="score">
-			<p>{$gameStore.game.team1Score}</p>
-		</div>
-		<div class="timer">
-			<Timer timeLeft={$gameStore.game.timeLeft} />
-		</div>
-		<div class="score">
-			<p>{$gameStore.game.team2Score}</p>
-		</div>
-		<div class="image">
-			<img src={$gameStore.icons.team2} alt="Team 2 Logo" />
-		</div>
-	</div>
-	<div class="bo-scores">
-		<div>
-			<div class="score-boxes">
-				{#each Array.from({ length: $gameStore.serie.nbOfMatch }, (_, index) => index) as game}
-					<div class="score-box {game < $gameStore.serie.team1Score ? 'filled' : ''}" />
-				{/each}
-			</div>
-			<div class="text"><p>{$gameStore.game.title}</p></div>
-			<div class="score-boxes">
-				{#each Array.from({ length: $gameStore.serie.nbOfMatch }, (_, index) => index) as game}
-					<div class="score-box {game < $gameStore.serie.team2Score ? 'filled' : ''}" />
-				{/each}
-			</div>
-		</div>
-	</div>
+  <div class="scores">
+    <div class="image">
+      <img src={$gameStore.icons.team1} alt="Team 1 Logo" />
+    </div>
+    <div class="score">
+      <p>{$gameStore.game.team1Score}</p>
+    </div>
+    <div class="timer">
+      <Timer timeLeft={$gameStore.game.timeLeft} />
+    </div>
+    <div class="score">
+      <p>{$gameStore.game.team2Score}</p>
+    </div>
+    <div class="image">
+      <img src={$gameStore.icons.team2} alt="Team 2 Logo" />
+    </div>
+  </div>
+  <div class="bo-scores">
+    <div>
+      <div class="score-boxes">
+        {#each Array.from({ length: $gameStore.serie.nbOfMatch }, (_, index) => index) as game}
+          {@const reverse = $gameStore.serie.nbOfMatch - 1 - game}
+          <div class="score-box {reverse < $gameStore.serie.team1Score ? 'filled' : ''}" />
+        {/each}
+      </div>
+      <div class="text"><p>{$gameStore.game.title}</p></div>
+      <div class="score-boxes">
+        {#each Array.from({ length: $gameStore.serie.nbOfMatch }, (_, index) => index) as game}
+          <div class="score-box {game < $gameStore.serie.team2Score ? 'filled' : ''}" />
+        {/each}
+      </div>
+    </div>
+  </div>
 </div>
 
 <style>
-	#container {
-		background-color: green;
-		font-family: 'Montserrat';
-		font-size: 40px;
-		width: fit-content;
-		padding: 0px;
-		margin: 0px;
-	}
-	.scores {
-		display: flex;
-		background-color: white;
-	}
-	.scores > * {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		height: 82px;
-	}
-	.scores > .score {
-		font-weight: 700;
-		min-width: 79px;
-	}
-	.scores > .timer {
-		color: white;
-		font-weight: 600;
-		min-width: 207px;
-		background-image: url('/timerBg.jpg');
-		background-repeat: no-repeat;
-	}
-	.scores > .image {
-		object-fit: cover;
-	}
-	.bo-scores {
-		display: flex;
-		justify-content: space-around;
-		align-items: center;
-	}
-	.bo-scores > * {
-		padding: 0px;
-		background-color: white;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		padding-top: 4px;
-		padding-bottom: 4px;
-	}
+  @font-face {
+    font-family: 'Thunder';
+    font-style: italic;
+    font-weight: 400;
+    src: url('/Thunder-LCItalic.ttf') format('truetype');
+  }
 
-	.score-boxes {
-		display: flex;
-	}
-	.score-boxes > div:nth-child(1) {
-		margin-left: 5px;
-	}
-	.bo-scores .text {
-		font-weight: bold;
-		font-size: 13px;
-		margin-left: 60px;
-		margin-right: 60px;
-	}
-	.score-box {
-		width: 60px;
-		height: 20px;
-		border: 3px solid black;
-		margin-right: 5px;
-	}
-	.filled {
-		background-color: black;
-	}
+  @font-face {
+    font-family: 'Thunder';
+    font-style: normal;
+    font-weight: 400;
+    src: url('/Thunder-MediumLC.ttf') format('truetype');
+  }
+
+  #container {
+    background-color: rghba(0, 0, 0, 0);
+    font-family: 'Montserrat';
+    font-size: 40px;
+    width: fit-content;
+    padding: 0px;
+    margin: 0px;
+  }
+  .scores {
+    display: flex;
+    background-color: white;
+  }
+  .scores > * {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 82px;
+  }
+  .scores > .score {
+    min-width: 79px;
+    color: white;
+    font-family: 'Thunder';
+  }
+  .scores :nth-child(2) {
+    background-color: #242f33;
+  }
+  .scores :nth-child(3) {
+    font-style: italic;
+  }
+  .scores :nth-child(4) {
+    background-color: #5e4d45;
+  }
+  .scores > .timer {
+    color: black;
+    font-weight: 600;
+    min-width: 207px;
+    background-repeat: no-repeat;
+  }
+  .scores > .image {
+    object-fit: cover;
+  }
+  .bo-scores {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+  .bo-scores > * {
+    padding: 0px;
+    background: linear-gradient(
+      to right,
+      rgba(255, 255, 255, 0.5) 0%,
+      rgba(255, 255, 255, 1) 5%,
+      rgba(255, 255, 255, 1) 95%,
+      rgba(255, 255, 255, 0.5)
+    );
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top: 4px;
+    padding-bottom: 4px;
+  }
+
+  .score-boxes {
+    display: flex;
+  }
+  .score-boxes > div:nth-child(1) {
+    margin-left: 5px;
+  }
+  .bo-scores .text {
+    font-weight: bold;
+    font-size: 13px;
+    margin-left: 60px;
+    margin-right: 60px;
+  }
+  .score-box {
+    background-color: #c4c4c4;
+    width: 25px;
+    height: 10px;
+    margin-right: 25px;
+  }
+  .bo-scores > div :nth-child(1) .filled {
+    background-color: #242f33;
+  }
+  .bo-scores > div :nth-child(3) .filled {
+    background-color: #5e4d45;
+  }
+
+  /* .filled { -->
+  <!--   background-color: black; -->
+  <!-- } */
 </style>
