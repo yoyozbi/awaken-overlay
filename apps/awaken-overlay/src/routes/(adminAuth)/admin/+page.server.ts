2375ac3ec9f424774ae4ea00f13bc12a7cc229d8
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-03-20 16:07:22 +0100

    made teams deletable

--- apps/awaken-overlay/src/routes/(adminAuth)/admin/+page.server.ts
+++ apps/awaken-overlay/src/routes/(adminAuth)/admin/+page.server.ts
@@ -16,9 +16,20 @@ export const load = (async () => {
 const deleteUserSchema = object({
 	userId: string().required()
 });
+
+const deleteTeamSchema = object({
+	teamId: string().required()
+});
+
 type deleteUser = InferType<typeof deleteUserSchema>;
+type deleteTeam = InferType<typeof deleteTeamSchema>;
+
 export const actions = {
 	deleteUser: async (event) => {
+		if (!event.locals.user || !event.locals.user.isAdmin) {
+			return fail(403, { error: 'Unauthorized' });
+		}
+
 		const formData = Object.fromEntries(await event.request.formData());
 		let data: deleteUser;
 		try {
@@ -30,6 +41,25 @@ export const actions = {
 		await db.authUser.delete({
 			where: {
 				id: data.userId
+			}
+		});
+	},
+	deleteTeam: async (event) => {
+		if (!event.locals.user || !event.locals.user.isAdmin) {
+			return fail(403, { error: 'Unauthorized' });
+		}
+
+		const formData = Object.fromEntries(await event.request.formData());
+		let data: deleteTeam;
+		try {
+			data = await deleteTeamSchema.validate(formData);
+		} catch (e) {
+			return fail(400, { error: 'Missing teamId' });
+		}
+
+		await db.team.delete({
+			where: {
+				id: data.teamId
 			}
 		});
 	}
